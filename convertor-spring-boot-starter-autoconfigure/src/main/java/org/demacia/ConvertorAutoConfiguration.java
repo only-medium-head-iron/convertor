6bc2856c46bfbce381609161c97710dc1f7dd002
only-medium-head-iron <296405966@qq.com>
2025-03-22 10:55:48 +0800

    convertor-start

---
+++ convertor-spring-boot-starter-autoconfigure/src/main/java/org/demacia/ConvertorAutoConfiguration.java
@@ -0,0 +1,12 @@
+package org.demacia;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hepenglin
+ * @since 2024/11/24 22:11
+ **/
+@Configuration
+public class ConvertorAutoConfiguration {
+
+}
